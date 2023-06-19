ccdc99319599d3244fc56bb1f843325f2bee919e
Walter Fabri <walterfabri18@gmail.com>
2023-06-19 18:56:35 -0300

    configurando e testando sequelize

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeControler {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Maria',
+      sobrenome: 'Fabri',
+      email: 'maria@gmail.com',
+      idade: 22,
+      peso: 57,
+      altura: 1.7,
     });
+    res.json(novoAluno);
   }
 }
 
